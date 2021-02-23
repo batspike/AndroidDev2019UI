e3160bcb05f23566192a0e6cc811ef33766b27b4
Sam Lau <samlau.sam@gmail.com>
2021-02-23 09:09:26 +0800

    initial clean up, simplified app

--- app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
@@ -1,42 +1,14 @@
 package com.samcancode.android.userinterface
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
-    // This event is triggered when this activity (MainActivity) is created.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        // FloatingActionButton (fab) is the email icon that is floating on the screen.
-        // Snackbar is a pop up bar shown for a few seconds and goes away. Here we setup the
-        // fab to trigger the snackbar when clicked.
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items in menu_main to the action bar (toolbar) if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
