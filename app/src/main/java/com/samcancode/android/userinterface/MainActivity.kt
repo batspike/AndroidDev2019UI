27b6202c39156219c0945e4b1458f111eb4d2a45
Sam Lau <samlau.sam@gmail.com>
2021-02-23 14:35:31 +0800

    adding TextView widget to LinearLayout programatically

--- app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.samcancode.android.userinterface
 
+import android.app.ActionBar
+import android.graphics.Color
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        // programatically add 3 TextView widgets
+        addTextView("Hello")
+        addTextView("From")
+        addTextView("Android")
+
+    }
+
+    private fun addTextView(label:String) {
+        val view = TextView(this)
+        view.text = label
+        view.textSize = 28f  // 28dp
+        view.setTextColor(Color.parseColor("#FF0000"))
+        findViewById<LinearLayout>(R.id.linearLayout).addView(view)
     }
 
 }
