b04c599a263876788f17406b2573dd2aacb0ace8
Sam Lau <samlau.sam@gmail.com>
2021-02-23 08:47:54 +0800

    at end of chapter 1

--- app/src/main/java/com/samcancode/android/userinterface/FirstFragment.kt
+++ app/src/main/java/com/samcancode/android/userinterface/FirstFragment.kt
@@ -14,9 +14,10 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
+            inflater: LayoutInflater,
+            container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+        ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
--- app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
@@ -9,11 +9,15 @@ import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
+    // This event is triggered when this activity (MainActivity) is created.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        // FloatingActionButton (fab) is the email icon that is floating on the screen.
+        // Snackbar is a pop up bar shown for a few seconds and goes away. Here we setup the
+        // fab to trigger the snackbar when clicked.
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
@@ -21,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items in menu_main to the action bar (toolbar) if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
