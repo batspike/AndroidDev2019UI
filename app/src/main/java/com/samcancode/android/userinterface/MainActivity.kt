2ab0fccac506b1ee0e2215d7d377bb09c0ab3014
batspike <samlau.sam@gmail.com>
2021-03-16 10:29:01 +0800

    chapter 4 completed

--- app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
+++ app/src/main/java/com/samcancode/android/userinterface/MainActivity.kt
@@ -1,43 +1,39 @@
 package com.samcancode.android.userinterface
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var container:LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<Button>(R.id.button_click_me).setOnClickListener{
-            val userName = findViewById<EditText>(R.id.nameInput).text.toString()
-            val password = findViewById<EditText>(R.id.passwordInput).text.toString()
-            // using string resouce allow use to provide localisation to our message
-            val message = getString(R.string.login_message, userName, password)
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener{
+            displayImageAsset("monster01.webp")
+        }
 
-            // Toast messages can be suppressed by user via the O/S. So they are
-            // not ideal for display important info specifically in our app. Better to use snackbar.
-            // Snackbar should be used with CoordinatorLayout component as its root element, or
-            // within a child of the CoordinatorLayout. Snackbar will automatically reference
-            // itself up to the parent that is a CoordinatorLayout. Snackbar also can have a
-            // button for additional functionality.
-//            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-            Snackbar.make(it, message, Snackbar.LENGTH_LONG)
-                    .setAction("Click Me", { showAnotherMessage() })
-                    .show()
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener{
+            displayImageAsset("monster02.webp")
+        }
+
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener{
+            displayImageAsset("monster03.webp")
         }
     }
 
-    private fun showAnotherMessage() {
-        Toast.makeText(this, "You Clicked!", Toast.LENGTH_LONG).show()
+    private fun displayImageAsset(fileName:String) {
+        assets.open(fileName).use {
+            val drawable = Drawable.createFromStream(it, null)
+            val monsterImage = findViewById<ImageView>(R.id.monsterImage)
+            monsterImage.setImageDrawable(drawable)
+        }
     }
-
 }
